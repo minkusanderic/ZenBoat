4ce003ceb049a762cdf9a82a619838d5a5bdf8ec
TyRawr <unknown-user@fe80::e1e8:6a18:732a:9f2f%13>
2013-11-06 19:43:32 -0700

    Adding art

--- Main/PreFabs/Boat.cs
+++ Main/PreFabs/Boat.cs
@@ -17,9 +17,9 @@ namespace Main
 			ent.Name = "Boat";
 			ent.tag("boat", "pushable");
 						    
-			var m = ent.attachComponent(new ModelComponent("/Application/resources/Water.mdx"));
-			//m.scale = new Vector3(50, 50, 50);
-			m.scale = new Vector3(5, 5, 5);
+			var m = ent.attachComponent(new ModelComponent("/Application/resources/boat2.mdx"));
+			m.scale = new Vector3(50, 50, 50);
+			//m.scale = new Vector3(5, 5, 5);
 			ent.attachComponent( new RigidBody(m.scale.X) );
 			ent.attachComponent( new FrictionController(.25f) );
 			ent.attachComponent( new Core.SimpleAction(get_done));
