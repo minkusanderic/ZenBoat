45ab019cd16ee65ffcd8be323ab696d1a65ff33b
Matt Carroll <tayopi8#gmail.com>
2013-11-09 17:26:24 -0700

    Added the current prefab

---
+++ Main/PreFabs/Current.cs
@@ -0,0 +1,20 @@
+using System;
+using Core;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Main
+{
+	public class Current
+	{
+		public Current ()
+		{
+		}
+		
+		public static void create(Entity ent, Dictionary<String, String> options)
+		{
+		}
+	}
+}
+
