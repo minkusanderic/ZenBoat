26781d0550e4c66e8cddfc3ae60265449d5d58bd
Eric Hurst <minkusanderic@gmail.com>
2013-11-16 18:50:36 -0700

    Added correct whirlpool

--- Main/PreFabs/Whirlpool.cs
+++ Main/PreFabs/Whirlpool.cs
@@ -16,24 +16,37 @@ namespace Main
 		
 		public static void create(Entity ent, Dictionary<String, String> options)
 		{
-			ent.tag("whirlpool", "cube");
+			ent.tag("whirlpool");
 		
+			int width = Convert.ToInt32(options["width"]);
+			int height = Convert.ToInt32(options["height"]);
+			int strength = Convert.ToInt32(options["strength"]);
 			
 			var m = ent.attachComponent(new SpriteComponent("/Application/Assets/Whirpoolsmall.png"));
-			//m.scale = new Vector3(100, 100, 100);
-			RigidBody rb = new RigidBody( 10 );
-			rb.body = new PhysicsBody();
+			//m.scale = new Vector2(.5f, .5f);
+			RigidBody rb = new RigidBody( Math.Min(width, height)/2 );
+			ent.attachComponent(rb);
 			rb.is_static = true;
 			
-
-			//rb.makeTrigger();
-			ent.attachComponent( rb );
 			
-			//rb.makeTrigger();
+			ent.attachComponent(new Core.SimpleController(() => {
+				List<Entity> targets = SceneManager.Instance.FindEntitiesByTag("pushable").ToList();
+				foreach(var obj in targets)
+				{
+					Vector2 dir = ent.Transform.Position - obj.Transform.Position;
+					float magnitude = ((float)strength)/dir.Length();
+					if(dir.Length() < (Math.Min(width, height) * 2.5f))
+					{
+						obj.FindComponent<RigidBody>().applyForce(dir * magnitude);
+					}
+				}
+			}));
 			
-			
-			List<Entity> targets = SceneManager.Instance.FindEntitiesByTag("pushable").ToList();
-			ent.attachComponent( new WhirlpoolTrigger( targets ) );
+			ent.attachComponent(new SimpleTrigger(SceneManager.Instance.FindEntitiesByTag("pushable").ToList(), 
+			                                      (t) => {
+														t.Destroy();
+													}));
+			//ent.attachComponent( new WhirlpoolTrigger( targets ) );
 		}
 	}
 }
