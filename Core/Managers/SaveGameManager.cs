dba736f2534eca42ea38ff3c667c6ea47721dfde
Tyler <unknown-user@fe80::716b:ddd3:9c9:d006%11>
2013-12-01 16:47:46 -0700

    Researching in-game-purchases modified the SaveGameManager to support writing strings, AND ONLY STRINGS BWAHAHAHAH

--- Core/Managers/SaveGameManager.cs
+++ Core/Managers/SaveGameManager.cs
@@ -41,11 +41,11 @@ namespace Core
 		private static XmlDocument xml_doc;
 		
 		private static string save_game_file_path = @"/Documents/saveGame.xml";
-		
+		private static Dictionary<string , string> strings;
 		//---------------------//
 		private static string currentLevel;
 		private static string lastTimePlayed;
-		private static List<string> strings = new List<string>();
+		//private static List<string> strings = new List<string>();
 		
 		//private static List<Level> levels = new List<Level>();
 		//---------------------//
@@ -61,35 +61,79 @@ namespace Core
 		public static bool init()
 		{
 			_init = true;
+			strings = new Dictionary<string, string>();
 			
-			const string DATA = "This is some data";
- 
-			byte[] persistentData = PersistentMemory.Read();
-				 
-			byte[] restoredData = persistentData.Take(DATA.Length * 2).ToArray();
-			string restoredString = System.Text.Encoding.Unicode.GetString(restoredData);
-				 
-			byte[] stringAsBytes = System.Text.Encoding.Unicode.GetBytes(DATA);
-				 
-			for(int i = 0; i < stringAsBytes.Length; i++)
-				persistentData[i] = stringAsBytes[i];
-			PersistentMemory.Write (persistentData);
-				 
-			using(FileStream fs = File.Open(save_game_file_path	,FileMode.OpenOrCreate))
+			const string DATA = 
+				"<SaveGame>" +
+				"\n\r\t<Strings>" +
+				"\n\r\t</Strings>" +
+				"</SaveGame>";
+			
+			FileStream fs = File.Open(save_game_file_path	,FileMode.OpenOrCreate);
+			xml_doc = new XmlDocument();
+			//xml_doc.Load( fs );
+			bool fileExists  = fs.Length != 0;
+			if ( !fileExists )
 			{
-				if(fs.Length == 0)
-				{
-					fs.Write(stringAsBytes,0,stringAsBytes.Length);
-				}
-				else
-				{
-					byte[] fileContents = new byte[fs.Length];	
-					fs.Read(fileContents,0,(int)fs.Length);
-				}
+				xml_doc.LoadXml( DATA );
+				xml_doc.Save(save_game_file_path);
 			}
+			else
+			{
+				xml_doc.Load( fs );
+				if ( xml_doc.FirstChild.FirstChild.Name == "Strings" )
+				{
+					foreach( XmlNode str in xml_doc.FirstChild.FirstChild.ChildNodes )
+					{
+						Console.WriteLine( str.Name );
+						string key,val;
+						key = val = "";
+						foreach( XmlNode node in str.ChildNodes )
+						{
+							if ( node.Name == "Key" )
+								key = node.InnerText;
+							else 
+							{
+								val = node.InnerText;
+							}
+						}
+						//Console.WriteLine("K:" + key + "\tV:" + val);
+						if ( key != "" && val != "" )
+						{
+							strings.Add(key , val);	
+						}
+					}
+				}	
+			}
+			fs.Close();	 
+			//SetString( "Poop" , "Pants" );
+			//xml_doc.Save( save_game_file_path );
 			return _init;
 		}
-					
+		
+		public static bool ContainsSaveString( string key )
+		{
+			return strings.ContainsKey(key);	
+		}
+		
+		public static void SetString( string key , string val )
+		{
+			if ( !strings.ContainsKey( key ) )
+			{
+				strings[key] = val;
+			}
+			XmlNode new_string = xml_doc.CreateNode( XmlNodeType.Element , "String" , "" );
+			XmlNode new_key = xml_doc.CreateNode( XmlNodeType.Element , "Key" , "" );
+			XmlNode new_val = xml_doc.CreateNode( XmlNodeType.Element , "Value" , "" );
+			
+			new_key.InnerText = key;
+			new_val.InnerText = val;
+			new_string.AppendChild( new_key );
+			new_string.AppendChild( new_val );
+			
+			xml_doc.FirstChild.FirstChild.AppendChild( new_string );
+		}
+		
 		public static bool loadGame()
 		{
 			if ( _init )
@@ -103,8 +147,7 @@ namespace Core
 		{
 			if ( _init )
 			{
-				
-				
+				xml_doc.Save( save_game_file_path );
 				return true;
 			}
 			return false;
