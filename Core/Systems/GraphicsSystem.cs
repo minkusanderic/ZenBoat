0fbbbb942b9a6c883c953d5750f65e12508996f7
tyrawr <ty.sundon@gmail.com>
2014-01-28 15:12:59 -0700

    Committing for Cel Shading

--- Core/Systems/GraphicsSystem.cs
+++ Core/Systems/GraphicsSystem.cs
@@ -61,10 +61,18 @@ namespace Core
 			}
 			
 			//cameraOffset += 1f;
-			return new Vector3(960.0f/2.0f + cameraOffset, 0f , (960.0f/2.0f)/ (1.77f * FMath.Tan(FMath.Radians(45.0f/2.0f)) ));
+			return new Vector3(960.0f/2.0f + cameraOffset,   0f  , (960.0f/2.0f)/ (1.77f * FMath.Tan(FMath.Radians(45.0f/2.0f)) ));
 		}
-		
-		
+		bool b = false;
+		Entity boat;
+		BasicProgram basic_program;
+		BasicParameters basic_parameters;
+		BasicProgramContainer basic_program_container;
+		ModelComponent model_component;
+		Texture2D texToon;
+		BasicTexture basic_texture;
+		BasicTextureContainer basic_texture_container;
+
 		public override void Update() // Render here
 		{
 			Timer.StartFrame();
@@ -109,12 +117,16 @@ namespace Core
 			graphics.SetCullFace( CullFaceMode.Back, CullFaceDirection.Ccw ) ;
 			graphics.Enable( EnableMode.DepthTest ) ;
 			graphics.SetDepthFunc( DepthFuncMode.LEqual, true ) ;
-	
+			
 			
 			//  adjust position
 			
 			foreach(var model in models)
 			{
+				if ( model.parent.Name == "Boat" )
+				{
+					continue;
+				}
 				Matrix4 world = Matrix4.Identity ;
 				
 				if ( model.model.BoundingSphere.W != 0.0f ) {
@@ -136,6 +148,93 @@ namespace Core
 				model.model.Update() ;
 				model.model.Draw(graphics, program) ;
 			}
+			
+			#region
+			// boat init 
+			
+			if ( !b )
+			{
+				b = true;
+				boat = SceneManager.Instance.FindEntity("Boat");
+				model_component = (ModelComponent)boat.FindComponent<ModelComponent>();
+				
+
+				basic_program = new BasicProgram("/Application/shaders/Basic.cgx" , null , null );
+								//basic_parameters = new BasicParameters();
+				BasicParameters bparameters = basic_program.Parameters ;
+				bparameters.Enable( BasicEnableMode.Lighting, true ) ;
+				bparameters.Enable( BasicEnableMode.Fog, true ) ;
+	
+				bparameters.SetProjectionMatrix( ref proj ) ;
+				bparameters.SetViewMatrix( ref view ) ;
+				bparameters.SetLightCount( 2 ) ;
+				bparameters.SetLightDirection( 0, ref litDirection ) ;
+				bparameters.SetLightDiffuse( 0, ref litColor ) ;
+				bparameters.SetLightSpecular( 0, ref litColor ) ;
+				bparameters.SetLightDirection( 1, ref litDirection2 ) ;
+				bparameters.SetLightDiffuse( 1, ref litColor2 ) ;
+				bparameters.SetLightSpecular( 1, ref litColor2 ) ;
+				bparameters.SetLightAmbient( ref litAmbient ) ;
+				bparameters.SetFogRange( -1.0f, 5000.0f ) ;
+				bparameters.SetFogColor( ref fogColor ) ;
+				//basic_program.SetAttributeBinding( 0 , "a_Position" );
+				//basic_program.SetAttributeBinding( 1 , "a_Normal" );
+				basic_program_container = new BasicProgramContainer( bparameters );
+				basic_program_container.Add( "Basic" , basic_program );
+				model_component.model.BindPrograms( basic_program_container );
+				
+				texToon = new Texture2D( "/Application/Assets/toon.png", false ) ;
+        		//texToon.SetFilter( Sce.PlayStation.Core.Graphics.TextureFilterMode.Nearest,
+                          // Sce.PlayStation.Core.Graphics.TextureFilterMode.Nearest,
+                          // Sce.PlayStation.Core.Graphics.TextureFilterMode.Disabled );
+				//texToon.
+				basic_texture_container = new BasicTextureContainer();
+				basic_texture_container.Add("ToonTex" , texToon );
+				model_component.model.BindTextures( basic_texture_container );
+				
+				//Console.WriteLine("tex[0]" +	model_component.model.Textures[0].Texture. );
+			}
+			#endregion	// Boat Init
+			// boat every frame
+			{	// declare new scope
+			if ( basic_program == null )
+				Console.WriteLine("Que?");
+			Matrix4 world = Matrix4.Identity ;
+			if (model_component .model.BoundingSphere.W != 0.0f ) {
+				Vector3 scale = model_component.scale;
+					
+				Vector3 pos = new Vector3(model_component.parent.Transform.Position.X, model_component.parent.Transform.Position.Y, model_component.parent.Transform.Z);
+					
+				//Console.WriteLine( model.parent.Name+"--" + pos );
+				world *= Matrix4.Translation(pos) ;
+				world *= Matrix4.Scale( scale.X, scale.Y, scale.Z ) ;
+				world *= Matrix4.RotationX(FMath.Radians(90.0f));
+				world *= Matrix4.RotationY (-model_component.parent.Transform.Rotation.Angle(Vector2.UnitX));
+					
+			}	
+			basic_program.Parameters.SetProjectionMatrix( ref proj );//					bparameters.SetProjectionMatrix( ref proj ) ;
+			basic_program.Parameters.SetViewMatrix( ref view );//	bparameters.SetViewMatrix( ref view ) ;
+			model_component.model.SetWorldMatrix( ref world );
+			model_component.model.Update() ;
+			//Vector3 localLightDir = new Vector3( 0f , 1f , 0f );
+			//basic_program.SetUniformValue( basic_program.FindUniform("LocalLightDirection") , ref localLightDir );
+				
+			//Matrix4 worldInverse = world.Inverse();
+			//Vector4 localEye4 = worldInverse * (new Vector4( camera_pos, 1.0f ));
+        	//Vector3 localEye = new Vector3( localEye4.X, localEye4.Y, localEye4.Z);
+			//basic_program .SetUniformValue( basic_program.FindUniform( "EyePosition" ), ref localEye );
+				
+			//model_component.model.SetWorldMatrix( ref world ) ;
+			//model_component.model.Animate( .01f ) ;
+			//model_component.model.Update() ;
+			//Vector4 poss = new Vector4(model_component.parent.Transform.Position.X, model_component.parent.Transform.Position.Y, model_component.parent.Transform.Z, 1f);
+			
+			//basic_program.SetUniformValue( basic_program.FindUniform("a_Position") , ref poss );
+			
+			model_component.model.Draw( graphics , basic_program );
+				
+			}
+			// End boat every frame
 			
 			foreach(var sprite in sprites)
 			{
