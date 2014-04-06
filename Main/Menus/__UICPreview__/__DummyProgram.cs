c7e3be324bb44f29ab1ce6d2cf787615b34e86de
Matt Carroll <tayopi8@gmail.com>
2014-04-05 23:24:26 -0600

    Fixed the duck in the store

--- Main/Menus/__UICPreview__/__DummyProgram.cs
+++ Main/Menus/__UICPreview__/__DummyProgram.cs
@@ -25,7 +25,7 @@ namespace Preview
             graphics = new GraphicsContext(960,544,PixelFormat.Rgba,PixelFormat.Depth16,MultiSampleMode.None);
             UISystem.Initialize(graphics);
 
-            Ending scene = new Ending();
+            Select scene = new Select();
             SetupListNum(scene.RootWidget);
             scene.SetWidgetLayout(LayoutOrientation.Horizontal);
             UISystem.SetScene(scene);
